e7bc1f82c59535b3a66d418331fec118c11d2d17
niharika8059 <niharika.raj@1mg.com>
2020-05-27 23:02:28 +0530

    Fixed notification message for ble/loc enabled/disable on boot

--- app/src/main/java/nic/goi/aarogyasetu/background/BluetoothScanningService.java
+++ app/src/main/java/nic/goi/aarogyasetu/background/BluetoothScanningService.java
@@ -246,8 +246,7 @@ public class BluetoothScanningService extends Service implements AdaptiveScanHel
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
         serviceRunning = true;
-        Notification notification = getNotification(Constants.NOTIFICATION_DESC);
-        startForeground(NOTIF_ID, notification);
+        configureNotification();
         mAdaptiveScanHelper = new AdaptiveScanHelper(this);
         mGattServer.onCreate(BluetoothScanningService.this);
         mGattServer.addGattService();
@@ -257,6 +256,18 @@ public class BluetoothScanningService extends Service implements AdaptiveScanHel
         registerLocationStateListener();
         Logger.d(TAG, "onStartCommand service started");
         return START_STICKY;
+    }
+
+    private void configureNotification() {
+        Notification notification;
+        if (!CorUtility.isLocationOn(CoronaApplication.instance.getContext())) {
+            notification = getNotification(Constants.PLEASE_ALLOW_LOCATION);
+        } else if (!CorUtility.isBluetoothAvailable()) {
+            notification = getNotification(Constants.PLEASE_ALLOW_BLUETOOTH);
+        } else {
+            notification = getNotification(Constants.NOTIFICATION_DESC);
+        }
+        startForeground(NOTIF_ID, notification);
     }
 
     private Notification getNotification(String notificationDescText) {
